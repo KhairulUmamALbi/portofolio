99f06789a2a1acf8e1da2c67f2ef2ff20e4f06ff
Umam <albikhairul212@gmail.com>
2023-05-09 11:42:56 +0700

    add new section but has trouble in html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
+    fonFamily: {
+      sans: ['ui-sans-serif', 'system-ui'],
+      serif: ['ui-serif', 'Georgia'],
+      mono: ['ui-monospace', 'SFMono-Regular'],
+      display: ['Oswald'],
+      body: ['Open Sans'],
+    },
     container: {
       center: true,
       padding: '16px',
